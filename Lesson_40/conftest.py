4ee96844ef0ed346c6f0919cdea6b6be28ef0989
Vitaliy Zamirovskiy <vitaliy.zamirovskiy@nangu.tv>
2026-05-20 20:49:28 +0200

    Добавлен CI/CD и GitHub Actions

--- Lesson_40/conftest.py
+++ Lesson_40/conftest.py
@@ -9,16 +9,16 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     options = Options()
 
-    # prefs = {
-    #     "credentials_enable_service": False,  # Отключаем предложение сохранить пароль
-    #     "profile.password_manager_enabled": False,  # Отключаем сам менеджер паролей
-    #     "profile.password_manager_leak_detection": False,  # Отключаем проверку утечек паролей
-    #     "safebrowsing.enabled": False  # Отключаем антивирус (Safe Browsing)
-    # }
-    #
-    # options.add_experimental_option("prefs", prefs)
+    prefs = {
+        "credentials_enable_service": False,  # Отключаем предложение сохранить пароль
+        "profile.password_manager_enabled": False,  # Отключаем сам менеджер паролей
+        "profile.password_manager_leak_detection": False,  # Отключаем проверку утечек паролей
+        "safebrowsing.enabled": False  # Отключаем антивирус (Safe Browsing)
+    }
 
-    options.add_argument("--headless=new")  # Режим без визуального отображения браузера
+    options.add_experimental_option("prefs", prefs)
+
+    # options.add_argument("--headless=new")  # Режим без визуального отображения браузера
     options.add_argument("--window-size=1920,1080")  # Установка размера окна для корректной работы в headless режиме
 
     options.add_argument("--no-sandbox")
